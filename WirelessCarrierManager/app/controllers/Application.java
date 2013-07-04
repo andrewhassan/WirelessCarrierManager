d33afe206ebf2db6a9f732c50a31492c5dc26df5
Andrew Hassan <andrewihassan@gmail.com>
2013-07-04 17:22:19 -0400

    Manage controller and page created

--- WirelessCarrierManager/app/controllers/Application.java
+++ WirelessCarrierManager/app/controllers/Application.java
@@ -1,16 +1,35 @@
 package controllers;
 
-import play.*;
+import models.*;
+import com.avaje.ebean.Ebean;
 import play.mvc.*;
-
 import views.html.*;
-import views.html.defaultpages.error;
 
 public class Application extends Controller {
 
-	public static Result index() {
-		//return ok(index.render("You're already connected!"));
+	public static Result index() {		
 		return ok(index.render("Test"));
+	}
+	
+	public static Result saveCarrier(String carrierName) {
+		Carrier testCarrier = new Carrier();
+		testCarrier.carrierName = carrierName;
+		testCarrier.plans = null;
+		Ebean.save(testCarrier);
+		return ok("" + testCarrier.carrierId);
+	}
+	
+	public static Result deleteCarrier(Long carrierId) {
+		Carrier testCarrier = Ebean.find(Carrier.class, carrierId);
+		
+		try {
+			Ebean.delete(testCarrier);
+		}
+		catch (Exception e) {
+			return internalServerError(); 
+		}
+		return ok("Yay");
+		
 	}
 	
 	public static Result customTitle(String str) {
