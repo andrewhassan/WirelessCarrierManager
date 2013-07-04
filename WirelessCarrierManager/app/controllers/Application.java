f87d4d08df1c4b7be4ba00e335ad44a8154c95c4
Andrew Hassan <andrewihassan@gmail.com>
2013-07-04 13:28:11 -0400

    Got MySQL hooked up. Doing some testing. Will be using ebean for ORM

--- WirelessCarrierManager/app/controllers/Application.java
+++ WirelessCarrierManager/app/controllers/Application.java
@@ -4,11 +4,17 @@ import play.*;
 import play.mvc.*;
 
 import views.html.*;
+import views.html.defaultpages.error;
 
 public class Application extends Controller {
-  
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
-  
+
+	public static Result index() {
+		//return ok(index.render("You're already connected!"));
+		return ok(index.render("Test"));
+	}
+	
+	public static Result customTitle(String str) {
+		return ok(index.render(str));
+	}
+
 }
